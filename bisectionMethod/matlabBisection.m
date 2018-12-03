27f0819f836dc27a93ff7ebb973717fa4cee5396
yasin baykal <ynkabasaliy@gmail.com>
2018-12-03 21:07:47 +0300

    Create matlabBisection.m

---
+++ bisectionMethod/matlabBisection.m
@@ -0,0 +1,59 @@
+clear all;clc;
+
+xm=0;
+xl = input("xl degeri:");
+xr = input("xr degeri:");
+km = 0;
+n=0;
+kn = input("Maks deneme sayisi:");
+td = input("TD degeri:");
+while (1==1)
+    if (n>=kn)
+        km=-1;
+        break
+    end
+    
+    yl = f(xl);
+    yr = f(xr);
+
+    if ((yl*yr)<0)
+        xm = (xl + xr)/2;
+        km = 1;
+    else
+        km = 0;
+        break
+    end
+
+    ym = f(xm);
+
+    if (sqrt(ym^2)<=td)
+        km = 1;
+        break
+    end
+
+    if ((yl*ym)<0)
+        xr=xm;
+        yr=ym;
+    else
+        xl=xm;
+        yl=ym;
+    end
+    n = n + 1;
+end
+
+if (km == 1)
+    fprintf("Bulunan kok:%.4f\n", xm);
+elseif (km == 0)
+    fprintf("Kok bulunamadi\n");
+else
+    fprintf("Maks deneme sayisinda istenilen kok bulunamadi\n");
+    fprintf("Hesaplanan son kok degeri:%.4f\n", xm);
+end
+
+
+
+function fx = f(x)
+    fx = (x^3) - 3.7*(x^2) + 6.25*x - 4.69;
+
+end
+
